e2a1cd436bf0f4b8e502ad26e7a21e8672b7de14
sagitario <sagitario@a09178c5-0f50-412e-aa4a-0aec1a98dccb>
2010-09-24 11:04:51 +0000

    git-svn-id: http://svn.dsource.org/projects/visuald/trunk@80 a09178c5-0f50-412e-aa4a-0aec1a98dccb

--- visuald/propertypage.d
+++ visuald/propertypage.d
@@ -580,9 +580,9 @@ class DebuggingPropertyPage : ProjectPropertyPage
 		AddControl("Command",           mCommand = new Text(mCanvas));
 		AddControl("Command Arguments", mArguments = new Text(mCanvas));
 		AddControl("Working Directory", mWorkingDir = new Text(mCanvas));
-		AddControl("",                  mAttach = new CheckBox(mCanvas, "Attach to runnng process"));
+		AddControl("",                  mAttach = new CheckBox(mCanvas, "Attach to running process"));
 		AddControl("Remote Machine",    mRemote = new Text(mCanvas));
-		AddControl("Debugger",          mDebugEngine = new ComboBox(mCanvas, [ "Visual Studio", "MaGo" ], false));
+		AddControl("Debugger",          mDebugEngine = new ComboBox(mCanvas, [ "Visual Studio", "Mago" ], false));
 	}
 
 	override void SetControls(ProjectOptions options)
