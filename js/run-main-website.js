e6f960f902b78d5c4681e19d5024ff93b83f3f06
Rainer Schuetze <r.sagitario@gmx.de>
2013-09-22 15:41:48 +0200

    add dlang.org pages with Visual D in navigation bar

---
+++ js/run-main-website.js
@@ -0,0 +1,19 @@
+/**
+Runnable examples functionality
+
+Copyright: 2012 by Digital Mars
+
+License:   http://boost.org/LICENSE_1_0.txt, Boost License 1.0
+
+Authors:   Damian Ziemba
+*/
+var mainPage = new Array();
+
+//The top one
+mainPage["27d33ceb10933b5541f5447f4bf1fc17"] = ["The D programming language. Modern convenience. Modeling power. Native efficiency."];
+
+//Automatic memory management makes for safe..
+mainPage["54f2095e83f8ef851a711db0a29b0e26"] = ["", "text.txt"];
+
+//Built-in linear and associative arrays, slices, and ranges make daily programmin
+mainPage["6c843684c0231b847f933e6d7b5a697a"] = ["The D programming language. Modern convenience. Modeling power. Native efficiency."];
