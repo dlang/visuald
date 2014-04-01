b5691ad9b50a55419143152396bff4d6e72e6296
aBothe <info@alexanderbothe.com>
2014-04-01 22:25:56 +0200

    [VDServer] Don't do Pango formatting in tooltips.

--- vdc/abothe/vdcomserver/VDServer.cs
+++ vdc/abothe/vdcomserver/VDServer.cs
@@ -54,6 +54,12 @@ namespace DParserCOMServer
 		public static readonly NodeToolTipContentGen Instance = new NodeToolTipContentGen();
 
 		private NodeToolTipContentGen() { }
+
+		protected override void AppendFormat (string content, StringBuilder sb, FormatFlags flags, double r = 0, double g = 0, double b = 0)
+		{
+			sb.Append (content);
+			//base.AppendFormat (content, sb, flags, r, g, b);
+		}
 	}
 
 	
