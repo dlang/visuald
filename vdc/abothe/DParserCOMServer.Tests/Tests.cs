937f5547f19564634829c3b700b843f35eccecc1
Alexander Bothe <info@alexanderbothe.com>
2018-10-27 17:06:17 +0200

    VDServer: Added GetDefinition() & GetParseError()-Tests. Reworked handling multi-threaded test setups.

--- vdc/abothe/DParserCOMServer.Tests/Tests.cs
+++ vdc/abothe/DParserCOMServer.Tests/Tests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Threading;
+using D_Parser.Dom;
 using D_Parser.Misc;
 using NUnit.Framework;
 
@@ -11,47 +12,66 @@ namespace DParserCOMServer.Tests
 	{
 		class VDServerDisposable : IDisposable
 		{
-			public readonly string subFolder;
-			public readonly string moduleFile;
-			private readonly string moduleCode;
-			private readonly SemaphoreSlim _parseFinishedSemaphore = new SemaphoreSlim(0);
+			private static volatile int _folderSuffix = 0;
+			readonly string _subFolder;
+			public readonly string FirstModuleFile;
+			public readonly string[] ModuleFileNames;
+			private readonly string[] _moduleCodes;
+			private readonly ManualResetEvent _parseFinishedSemaphore = new ManualResetEvent(false);
 
-			public VDServerDisposable(string moduleCode)
+			public VDServerDisposable(params string[] moduleCodes)
 			{
-				this.moduleCode = moduleCode;
+				_moduleCodes = moduleCodes;
+				ModuleFileNames = new string[moduleCodes.Length];
 				var tempDirectory = Path.GetTempPath();
-				subFolder = Path.Combine(tempDirectory, "vdserver_test");
-				Directory.CreateDirectory(subFolder);
-				moduleFile = Path.Combine(subFolder, "test.d");
+				_subFolder = Path.Combine(tempDirectory, "vdserver_test" + Interlocked.Increment(ref _folderSuffix));
+				Directory.CreateDirectory(_subFolder);
 
-				File.WriteAllText(moduleFile, moduleCode);
+				for(var fileIterator = 0; fileIterator < moduleCodes.Length; fileIterator++)
+				{
+					var file = Path.Combine(_subFolder, "test" + fileIterator + ".d");
+					ModuleFileNames[fileIterator] = file;
+					File.WriteAllText(file, moduleCodes[fileIterator]);
+				}
+				FirstModuleFile = ModuleFileNames.Length > 0 ? ModuleFileNames[0] : String.Empty;
 
 				GlobalParseCache.ParseTaskFinished += ParseTaskFinished;
 			}
 
 			private void ParseTaskFinished(ParsingFinishedEventArgs ea)
 			{
-				if (ea.Directory.StartsWith(subFolder))
-				{
-					_parseFinishedSemaphore.Release(1);
-				}
+				Console.WriteLine("Parse task finished: " + ea.Directory);
+				if (!ea.Directory.StartsWith(_subFolder))
+					return;
+
+				var moduleFiles = GlobalParseCache.EnumModulesRecursively(_subFolder).ConvertAll(module => module.FileName);
+				CollectionAssert.AreEquivalent(moduleFiles, ModuleFileNames);
+				_parseFinishedSemaphore.Set();
 			}
 
 			public IVDServer Initialize(uint flags = 0)
 			{
-				var instance = new VDServer();
-				instance.ConfigureSemanticProject(null, subFolder, "", "", "", flags);
-				Console.WriteLine("Waiting for " + subFolder + " to be parsed...");
-				Assert.That(_parseFinishedSemaphore.Wait(10000));
-				Console.WriteLine("Finished parsing " + subFolder);
-				instance.UpdateModule(moduleFile, moduleCode, 0);
+				IVDServer instance = new VDServer();
+				instance.ConfigureSemanticProject(null, _subFolder, "", "", "", flags);
+				Console.WriteLine("Waiting for " + _subFolder + " to be parsed...");
+				Assert.That(_parseFinishedSemaphore.WaitOne(10000));
+				Console.WriteLine("Finished parsing " + _subFolder);
+				for (int moduleIndex = 0; moduleIndex < ModuleFileNames.Length; moduleIndex++)
+				{
+					var file = ModuleFileNames[moduleIndex];
+					instance.UpdateModule(file, _moduleCodes[moduleIndex], 0);
+				}
+
 				return instance;
 			}
 
 			public void Dispose()
 			{
-				Console.WriteLine("Deleting folder " + subFolder);
-				Directory.Delete(subFolder, true);
+				GlobalParseCache.ParseTaskFinished -= ParseTaskFinished;
+				Console.WriteLine("Deleting folder " + _subFolder);
+				Directory.Delete(_subFolder, true);
+				if(_parseFinishedSemaphore.WaitOne(0))
+					Assert.That(GlobalParseCache.RemoveRoot(_subFolder));
 			}
 		}
 
@@ -72,7 +92,7 @@ void main() {}";
 			using (var vd = new VDServerDisposable(code))
 			{
 				var instance = vd.Initialize();
-				instance.GetTip(vd.moduleFile, 2, 7, 2, 7, 0);
+				instance.GetTip(vd.FirstModuleFile, 2, 7, 2, 7, 0);
 
 				string answer;
 				int remainingAttempts = 200;
@@ -88,7 +108,7 @@ void main() {}";
 		}
 
 		[Test]
-		public void GetSemanticExpensions()
+		public void GetSemanticExpansions()
 		{
 			var code = @"module test;
 void foo() {
@@ -105,7 +125,7 @@ class A(string mixinCode) {
 			using (var vd = new VDServerDisposable(code))
 			{
 				var instance = vd.Initialize(CompileFlags(VDServerEditorFlags.EnableMixinAnalysis));
-				instance.GetSemanticExpansions(vd.moduleFile, String.Empty, 4, 2, null);
+				instance.GetSemanticExpansions(vd.FirstModuleFile, String.Empty, 4, 2, null);
 
 				string answer;
 				int remainingAttempts = 200;
@@ -131,6 +151,53 @@ class A(string mixinCode) {
 					"classinfo:SPRP:object.TypeInfo_Class classinfo\aInformation about the dynamic type of the class"
 				};
 				CollectionAssert.AreEquivalent(expectedExpansions, receivedExpansions);
+			}
+		}
+
+		[Test]
+		public void GetDefinition()
+		{
+			var code_A = @"module test0;
+void foo();";
+			var code_B = @"module test1; import test0;
+void main() {
+foo();
+}";
+			using (var vd = new VDServerDisposable(code_A, code_B))
+			{
+				var instance = vd.Initialize();
+				instance.GetDefinition(vd.ModuleFileNames[1], 3, 0, 3, 1);
+
+				string targetFilename;
+				int startLine, startIndex, endLine, endIndex;
+				int remainingAttempts = 200;
+				do
+				{
+					Thread.Sleep(200 - remainingAttempts);
+					instance.GetDefinitionResult(out startLine, out startIndex,
+						out endLine, out endIndex, out targetFilename);
+				} while (targetFilename == "__pending__" && remainingAttempts-- > 0);
+
+				Assert.That(targetFilename, Is.EqualTo("EXTERN:" + vd.FirstModuleFile));
+				Assert.That(startLine, Is.EqualTo(2));
+			}
+		}
+
+		[Test]
+		public void GetParseErrors()
+		{
+			var code = @"module test0;
+void foo() {
+a.
+}";
+
+			using (var vd = new VDServerDisposable(code))
+			{
+				var instance = vd.Initialize();
+				instance.GetParseErrors(vd.FirstModuleFile, out var errors);
+				Assert.That(errors, Is.EqualTo(@"4,0,4,1:<Identifier> expected, } found!
+4,0,4,1:; expected, } found!
+"));
 			}
 		}
 	}
